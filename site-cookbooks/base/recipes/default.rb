a94eafe73e1358331eb67e4b19d477732e44efb4
Ikuo Degawa <degawa@ikuwow.com>
2014-12-21 12:21:29 +0900

    時刻設定とルートパスワードを追加

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -13,3 +13,27 @@
     end
 end
 
+=begin
+network_tools = %w{nslookup}
+network_tools.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+=end
+
+execute "rm -f /etc/localtime" do
+    not_if "test -L /etc/localtime"
+end
+link "Set localtime" do
+    link_type :symbolic
+    target_file "/etc/localtime"
+    to "/usr/share/zoneinfo/Asia/Tokyo"
+end
+
+user "Setting Root Password" do
+    username "root"
+    password "$1$xwKCp9Sr$FjkErIasrmWahenxL23af1" # root
+    action :modify
+end
+
