f62a718b63bd3c206752b8131de313d7c9c8fe60
Ikuo Degawa <degawa@ikuwow.com>
2014-12-22 13:44:38 +0900

    postgresql93をインストール

--- site-cookbooks/postgresql/recipes/default.rb
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -7,11 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
 bash "add_postgres_rpm" do
     code <<-EOC
     rpm -Uvh http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm
     sed -i -e "s/enabled *= *1/enabled=0/g" /etc/yum.repos.d/pgdg-93-centos.repo
     EOC
     creates "/etc/yum.repos.d/pgdg-93-centos.repo"
+end
+
+package "postgresql93" do
+    options "--enablerepo=pgdg93"
+    action :install
 end
