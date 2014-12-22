45f358de34dbb265306f24839bb365cf5f552b78
Ikuo Degawa <degawa@ikuwow.com>
2014-12-22 13:37:40 +0900

    postgresqlを入れるためのrpmパッケージをインストール
    enable=0

---
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: postgresql
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+bash "add_postgres_rpm" do
+    code <<-EOC
+    rpm -Uvh http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm
+    sed -i -e "s/enabled *= *1/enabled=0/g" /etc/yum.repos.d/pgdg-93-centos.repo
+    EOC
+    creates "/etc/yum.repos.d/pgdg-93-centos.repo"
+end
